e3ea2da88e470d402114eaaf21fa9912e6dd10c9
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2019-03-28 00:26:46 -0700

    Add Weight Standardization

--- ImageNet-ResNet-TensorFlow/imagenet-resnet-gn.py
+++ ImageNet-ResNet-TensorFlow/imagenet-resnet-gn.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File: imagenet-resnet-gnwd.py
 
 import argparse
 import os
@@ -12,7 +11,7 @@ from tensorpack.callbacks import *
 from tensorpack.train import (
     TrainConfig, SyncMultiGPUTrainerReplicated, launch_train_with_config)
 from tensorpack.dataflow import FakeData
-from tensorpack.tfutils import argscope, get_model_loader
+from tensorpack.tfutils import argscope, get_model_loader, varreplace
 from tensorpack.utils.gpu import get_num_gpu
 
 from imagenet_utils import (
@@ -30,10 +29,26 @@ class Model(ImageNetModel):
 
     depth = 50
 
+    use_WS = False
+    """
+    Whether to use Weight Standardization (https://arxiv.org/abs/1903.10520)
+    """
+
     def get_logits(self, image):
-        num_blocks = {50:[3, 4, 6, 3], 101:[3,4,23,3]}[self.depth]
+
+        def weight_standardization(v):
+            if not self.use_WS:
+                return v
+            if (not v.name.endswith('/W:0')) or v.shape.ndims != 4:
+                return v
+            mean, std = tf.nn.moments(v, [0, 1, 2], keep_dims=True)
+            v = (v - mean) / (std + 1e-5)
+            return v
+
+        num_blocks = {50: [3, 4, 6, 3], 101: [3, 4, 23, 3]}[self.depth]
         block_func = resnet_bottleneck
-        with argscope([Conv2D, MaxPooling, GlobalAvgPooling], data_format=self.data_format):
+        with argscope([Conv2D, MaxPooling, GlobalAvgPooling], data_format=self.data_format), \
+                varreplace.remap_variables(weight_standardization):
             return resnet_backbone(
                 image, num_blocks, resnet_group, block_func)
 
@@ -98,12 +113,14 @@ if __name__ == '__main__':
     parser.add_argument('--fake', help='use fakedata to test or benchmark this model', action='store_true')
     parser.add_argument('--eval', action='store_true')
     parser.add_argument('--batch', default=256, type=int, help='total batch size.')
-    parser.add_argument('--depth', type=int, default=50)
-    parser.add_argument('--logdir', default='train_log/ResNet50-GN')
+    parser.add_argument('-d', '--depth', type=int, default=50, choices=[50, 101])
+    parser.add_argument('--logdir', default='train_log/ResNet-GN')
+    parser.add_argument('--WS', action='store_true', help='Use Weight Standardization')
     args = parser.parse_args()
 
     model = Model()
     model.depth = args.depth
+    model.use_WS = args.WS
     if args.eval:
         batch = 128    # something that can run on one gpu
         ds = get_imagenet_dataflow(args.data, 'val', batch)
@@ -114,6 +131,11 @@ if __name__ == '__main__':
         else:
             logger.set_logger_dir(args.logdir, 'd')
 
+        try:
+            from tensorpack.tfutils import collect_env_info
+            logger.info("\n" + collect_env_info())
+        except Exception:
+            pass
         config = get_config(model, fake=args.fake)
         if args.load:
             config.session_init = get_model_loader(args.load)
